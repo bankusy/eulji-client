97bd694781a2a723490de29615bc151768c9a23b
bankusy <dlwodud821@gmail.com>
2026-01-13 10:15:23 +0900

    init: Initialize Project configuration and cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com"
+      },
+      {
+        protocol: "https",
+        hostname: "assets.getliner.com"
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com"
+      },
+
+    ]
+  }
 };
 
 export default nextConfig;
